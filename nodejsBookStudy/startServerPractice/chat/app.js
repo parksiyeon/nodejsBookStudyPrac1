1e087758e04af572d1f4acca4298d4dd69bef0ad
parksiyeon <mgo_iiviiv@naver.com>
2023-11-20 15:26:24 +0900

    웹 소켓 프로토콜 예제(ws 모듈과 socket.io 모듈)

---
+++ nodejsBookStudy/startServerPractice/chat/app.js
@@ -0,0 +1,32 @@
+const http = require("http");
+const express = require("express");
+const app = express();
+
+const server = http.Server(app);
+const io = require("socket.io")(server);
+let users = [];
+
+server.listen(8080, () => {
+    console.log("8080포트에서 서버 실행 중. . .");
+});
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket) => {
+    let name = "";
+    socket.on("has connected", (username) => {
+        name = username;
+        users.push(username);
+        io.emit("has connected", { username: username, usersList: users });
+    });
+    socket.on("has disconnected", () => {
+        users.splice(users.indexOf(name), 1);
+        io.emit("has disconnected", {username: name, usersList: users});
+    })
+
+    socket.on("new message", (data) => {
+        io.emit("new message", data);
+    });
+});
